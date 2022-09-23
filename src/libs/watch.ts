81a70f0f4cf27718733cd1e46435f7cce1bda462
三木 <578595193@qq.com>
2022-09-23 16:56:13 +0800

    FEAT: add lodash support

--- src/libs/watch.ts
+++ src/libs/watch.ts
@@ -1,19 +1,20 @@
 import {effect} from './effect'
 import { computed } from './computed'
+import { isEqual, cloneDeep } from 'lodash-es'
 function watch(source: () => any, cb: (nv: any, ov: any) => void, options: any = null): () => void {
     options = {immediate: false, deep: false, ...options}
     let cValue = computed(source)
     let beforeValue: any = null
-    let currentValue: any = JSON.parse(JSON.stringify(cValue.value))
+    let currentValue: any = cloneDeep(cValue.value)
 
     if (options.immediate) {
         cb(currentValue, beforeValue)
     }
     const eff = effect(() => {
-        let preCurrentValue = options.deep ? JSON.parse(JSON.stringify(currentValue)) : currentValue
-        currentValue = options.deep ? JSON.parse(JSON.stringify(cValue.value)) : cValue.value
+        let preCurrentValue = options.deep ? cloneDeep(currentValue) : currentValue
+        currentValue = options.deep ? cloneDeep(cValue.value) : cValue.value
         if (options.deep) {
-            if (JSON.stringify(preCurrentValue) !== JSON.stringify(currentValue)) {
+            if (!isEqual(preCurrentValue, currentValue)) {
                 beforeValue = preCurrentValue
                 cb(currentValue, beforeValue)
             }
